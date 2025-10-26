f0b5def9b663ff37b5b3cad736ade6f0475ae3d3
melanie lepage <melanie.lepage86@hotmail.fr>
2025-10-26 14:41:21 +0100

    debug connection stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,8 @@
-if Rails.env.production?
-  Rails.configuration.stripe = {
-    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-    secret_key:      ENV['STRIPE_SECRET_KEY'],
-  }
-else
-  Rails.configuration.stripe = {
-    publishable_key: ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
-    secret_key:      ENV['STRIPE_TEST_SECRET_KEY'],
-  }
-end
+Rails.configuration.stripe = {
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'].presence || ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+  secret_key:      ENV['STRIPE_SECRET_KEY'].presence || ENV['STRIPE_TEST_SECRET_KEY']
+}
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+STRIPE_PUBLISHABLE_KEY = Rails.configuration.stripe[:publishable_key]
